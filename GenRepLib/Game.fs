383e8c09177a3824d130b9acccc06160cb7902cb
Phil-Brooks <phil@brookshome.co.uk>
2023-06-08 20:26:38 +0100

    start Game

---
+++ GenRepLib/Game.fs
@@ -0,0 +1,9 @@
+﻿namespace GenRepLib
+
+open FsChess
+
+module Game =
+    
+    let GetLastFens (gm:Game) =
+        //[2;3;5] indicates go to RAV at index 2, within this go to RAV at index 3 and then get item at index 5
+        ()
