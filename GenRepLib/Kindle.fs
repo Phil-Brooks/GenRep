aac2fa861f30f5d10ff5d9249497217a640ccca6
Phil-Brooks <phil@brookshome.co.uk>
2023-06-12 21:39:56 +0100

    started Kindle

---
+++ GenRepLib/Kindle.fs
@@ -0,0 +1,13 @@
+﻿namespace GenRepLib
+
+open System.IO
+open KindleChess
+
+module Kindle =
+    let mutable tfol = ""
+    let mutable ofol = ""
+    let WhiteOpf() =
+        let chs = Rep.WhiteChapters()
+        //TODO do something with
+        Book.genh chs[0] tfol (Rep.wfol()) ofol
+        ()
