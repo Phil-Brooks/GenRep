2771623154f1b305f91ef2e3c31adcca26519a32
Phil-Brooks <phil@brookshome.co.uk>
2023-06-13 16:33:03 +0100

    update cmd

--- Cmd/grob.bat
+++ Cmd/grob.bat
@@ -1,5 +1,5 @@
 D:\Github\GenRep\Grob\bin\Release\net7.0\Grob.exe
 cd /d D:\Github\GenRep\Cmd
 kindlegen D:\Github\GenRep\Rep\White\Kindle\book.opf -o GrobWhite.mobi
-kindlegen D:\Github\GenRep\Rep\White\Kindle\book.opf -o GrobWhite.mobi
+kindlegen D:\Github\GenRep\Rep\Black\Kindle\book.opf -o GrobBlack.mobi
 pause
---
+++ Cmd/wb23.bat
@@ -0,0 +1,5 @@
+D:\Github\GenRep\GenRep\bin\Release\net7.0\GenRep.exe
+cd /d D:\Github\GenRep\Cmd
+kindlegen D:\Rep\2023\White\Kindle\book.opf -o W23.mobi
+kindlegen D:\Rep\2023\Black\Kindle\book.opf -o B23.mobi
+pause
